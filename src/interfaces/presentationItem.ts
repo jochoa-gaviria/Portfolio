bbbeff3dc759f3251475e97611b3411a156385c3
Juan David Ochoa <jochoa.gaviria@hotmail.com>
2023-07-03 14:58:27 -0500

    feat(interfaces) implement interfaces for all components

---
+++ src/interfaces/presentationItem.ts
@@ -0,0 +1,5 @@
+interface ipresentationItem {
+    text: string;
+}
+
+export type { ipresentationItem };
---
+++ src/interfaces/socialNetwork.ts
@@ -0,0 +1,7 @@
+interface isocialNetwork {
+    name: string;
+    icon: string;
+    url: string;
+}
+
+export type { isocialNetwork }
