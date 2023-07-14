fde717c7b9ea71c26ba12d683832872fe9e71092
Juan David Ochoa <jochoa.gaviria@hotmail.com>
2023-07-13 20:49:15 -0500

    feat(skills-component): complete skills components

---
+++ src/interfaces/progressWrapSkillItem.ts
@@ -0,0 +1,8 @@
+import type { ichartSkillItem } from './chartSkillItem'
+
+interface iprogressWrapSkillItem extends ichartSkillItem {
+    style: 1 | 2 | 3 | 4
+    group: 1 | 2
+}
+
+export type { iprogressWrapSkillItem }
